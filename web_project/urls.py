d6292015a1e60ca42d181ac55336092f2d39ea99
Ryan Byrne <rbyrne01@live.com>
2020-04-23 18:29:56 -0700

    version4

--- web_project/urls.py
+++ web_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .directories import views
 
 urlpatterns = [
     path('', views.index, name="index"),
